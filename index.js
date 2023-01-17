ec71ef604ee4c670aa745920793c0336ffdec90c
Sam <samvel_2002@list.ru>
2023-01-18 01:53:25 +0400

    trying to fix cors 9

--- index.js
+++ index.js
@@ -28,32 +28,32 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-// app.use(
-//   cors({
-//     origin: "https://social-media-front-rho.vercel.app",
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://social-media-front-rho.vercel.app",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
-const allowCrossDomain = function allowCrossDomain(req, res, next) {
-  console.log(">>>>>>>>>>");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-DAOMAKER, GOOGAPPUID, X-HACKERS, X-Parse-Master-Key, X-Parse-REST-API-Key, X-Parse-Javascript-Key, X-Parse-Application-Id, X-Parse-Client-Version, X-Parse-Session-Token, X-Requested-With, X-Parse-Revocable-Session, Content-Type, Cache-control, csrf-token, user-agent"
-  );
-  res.header("Access-Control-Expose-Headers", "Content-Disposition");
-  if ("OPTIONS" == req.method) {
-    // intercept OPTIONS method
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-};
+// const allowCrossDomain = function allowCrossDomain(req, res, next) {
+//   console.log(">>>>>>>>>>");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "X-DAOMAKER, GOOGAPPUID, X-HACKERS, X-Parse-Master-Key, X-Parse-REST-API-Key, X-Parse-Javascript-Key, X-Parse-Application-Id, X-Parse-Client-Version, X-Parse-Session-Token, X-Requested-With, X-Parse-Revocable-Session, Content-Type, Cache-control, csrf-token, user-agent"
+//   );
+//   res.header("Access-Control-Expose-Headers", "Content-Disposition");
+//   if ("OPTIONS" == req.method) {
+//     // intercept OPTIONS method
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// };
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
 
 // app.use(function (req, res, next) {
 //   //Enabling CORS
